29f5f8f468acb3f8f3a84ae126a6dbe38d83eafe
noel-pena <azulu88@hotmail.com>
2024-06-06 14:02:41 -0400

    structuring

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import pygame, sys, time
 from settings import *
+from sprites import BG
 
 class Game:
     def __init__ (self):
@@ -12,6 +13,8 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
         
+        BG(self.all_sprites)
+        
     def run(self):
         last_time = time.time()
         while True:
@@ -20,11 +23,16 @@ class Game:
             last_time = time.time()
             
             for event in pygame.event.get():
-                pygame.quit()
-                sys.exit()
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                    
+            self.all_sprites.draw(self.display_surface)
             
             pygame.display.update()
             self.clock.tick(FRAMERATE)
+
+
 
 if __name__ == '__main__':
     game = Game()
